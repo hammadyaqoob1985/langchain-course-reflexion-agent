876ce67f14243292353f196bc32089bdc63dcfbf
hammadyaqoob1985 <hammad.yaqoob1985@yahoo.co.uk>
2026-01-04 20:46:07 +0000

    added revise instructions and also execute tools

--- tool_executor.py
+++ tool_executor.py
@@ -16,7 +16,7 @@ def run_queries(search_queries: list[str], **kwargs):
 
 execute_tools = ToolNode(
     [
-        StructuredTool.from_function(run_queries, name=AnswerQuestion.__name__),
+        StructuredTool.from_function(run_queries, name=AnswerQuestion.__name__), 
         StructuredTool.from_function(run_queries, name=ReviseAnswer.__name__)
     ]
 
